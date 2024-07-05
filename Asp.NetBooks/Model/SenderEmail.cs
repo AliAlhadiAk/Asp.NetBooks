dd1531a2a6b8bbffc81e088628339b4b8ba8099d
AlialhadiAbokhalil <166250920+AliAlhadiAk@users.noreply.github.com>
2024-07-05 09:04:49 +0300

    Delete Asp.NetBooks/Model/SenderEmail.cs

--- Asp.NetBooks/Model/SenderEmail.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Asp.NetBooks.Model
-{
-    public static class SenderEmail
-    {
-        public static string Email { get; set; } = "alialhadiabokhalil@gmail.com";
-        public static string Password = "ASp.net_c#123";
-    }
-}
